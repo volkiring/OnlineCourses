70900bfbabe9f82feedf16855099a572f2c43ed2
stud <stud@07-409-17>
2025-05-29 10:55:19 +0300

    .

--- EfDbOnlineCourses/Models/Request.cs
+++ EfDbOnlineCourses/Models/Request.cs
@@ -2,7 +2,7 @@
 {
 	public class Request
 	{
-		public int Id { get; set; }
+		public string Id { get; set; }
 		public User User { get; set; }
 		public RequestType Type { get; set; }
 		public string? Message { get; set; }
